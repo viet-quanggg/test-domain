aa49691eb1036027a55b00ec7aa9e099b02086cb
viet-quangg <nguyenvietquang099@gmail.com>
2025-05-04 15:03:09 +0700

    fix build

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { Box, MantineProvider, Text } from '@mantine/core'
+import {  MantineProvider } from '@mantine/core'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
